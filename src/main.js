a2c9435f72b6a0998a7812298b21e0951053afdb
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-07-20 19:57:52 +0300

    final side of Performed task

--- src/main.js
+++ src/main.js
@@ -60,18 +60,16 @@ function addToDo() {
             selectorView.removeChild(todoContainer);
             counterFnc();
             saveTasks();
-        };
+        }
 
         checkBox.onclick = function () {
             if (checkBox.checked) {
                 selectorPerformed.appendChild(todoContainer);
-                selectorView.removeChild(todoContainer);
             } else {
-
                 selectorView.appendChild(todoContainer);
-                selectorPerformed.removeChild(todoContainer);
             }
-        };
+            saveTasks();
+        }
         counterFnc();
     } else {
         Swal.fire("The text box is empty")
@@ -173,6 +171,19 @@ function saveTasks() {
     }
     let JSONReadyArr = JSON.stringify(TasksOfarr);
     localStorage.setItem("tasks", JSONReadyArr);
+
+    let divsPerformed = selectorPerformed.getElementsByClassName('todoContainer');
+    let PreformedOfarr = [];
+    for (let i = 0; i < divsPerformed.length; i++) {
+        let arrTask = [];
+        arrTask[0] = divsPerformed[i].getElementsByClassName("todoText")[0].innerHTML;
+        arrTask[1] = divsPerformed[i].getElementsByClassName("todoCreatedAt")[0].innerHTML;
+        arrTask[2] = divsPerformed[i].getElementsByClassName("todoPriority")[0].innerHTML;
+        arrTask[3] = divsPerformed[i].getElementsByClassName("checkBox")[0].checked;
+        PreformedOfarr.push(arrTask);
+    }
+    let JSONReadyArrPerformed = JSON.stringify(PreformedOfarr);
+    localStorage.setItem("Performed", JSONReadyArrPerformed);
     localStorage.setItem("title", title.innerHTML);
 }
 
@@ -184,47 +195,67 @@ function init() {
     if (localStorage.tasks) {
         let TasksOfarr = JSON.parse(localStorage.getItem("tasks"));
         for (let i = 0; i < TasksOfarr.length; i++) {
-            const todoContainer = document.createElement("div"); //מסגרת לכל מטלה
-            const todoText = document.createElement("div"); //הטקסט של המטלה
-            const todoCreatedAt = document.createElement("div"); //הזמן שנרשמה המטלה
-            const todoPriority = document.createElement("div"); //עדיפות מ-1 עד 5
-            const deleteButton = document.createElement("button");
-            const checkBox = document.createElement("input");
-
-            todoContainer.className = "todoContainer";
-            todoText.className = "todoText";
-            todoCreatedAt.className = "todoCreatedAt";
-            todoPriority.className = "todoPriority";
-            deleteButton.className = "deleteButton";
-            checkBox.className = "checkBox";
-
-            todoText.innerHTML = TasksOfarr[i][0];
-            todoCreatedAt.innerHTML = TasksOfarr[i][1];
-            todoPriority.innerHTML = TasksOfarr[i][2];
-            const deleteIcon = document.createElement("i");
-            deleteIcon.className = "fa fa-trash";
-            deleteIcon.setAttribute("aria-hidden", "true");
-            deleteButton.appendChild(deleteIcon);
-            checkBox.setAttribute("type", "checkbox");
-            checkBox.checked = TasksOfarr[i][3];
-
-            todoContainer.append(todoText, todoCreatedAt, todoPriority, checkBox, deleteButton);
-            selectorView.appendChild(todoContainer);
-
-            deleteButton.onclick = function () {
-                deletedArr.push(todoContainer);
-                selectorView.removeChild(todoContainer);
-                counterFnc();
-            }
-            counterFnc();
+            createNewElemt(i, TasksOfarr, selectorView);
         }
     }
+    if (localStorage.Performed) {
+        let TasksOfarr = JSON.parse(localStorage.getItem("Performed"));
+        for (let i = 0; i < TasksOfarr.length; i++) {
+            createNewElemt(i, TasksOfarr, selectorPerformed);
+        }
+    }
+
 }
 
+//פעולה שיוצרת אלמנט, פעולת עזר לפעולה הקודמת
+function createNewElemt(i, TasksOfarr, selector) {
+    const todoContainer = document.createElement("div"); //מסגרת לכל מטלה
+    const todoText = document.createElement("div"); //הטקסט של המטלה
+    const todoCreatedAt = document.createElement("div"); //הזמן שנרשמה המטלה
+    const todoPriority = document.createElement("div"); //עדיפות מ-1 עד 5
+    const deleteButton = document.createElement("button");
+    const checkBox = document.createElement("input");
 
+    todoContainer.className = "todoContainer";
+    todoText.className = "todoText";
+    todoCreatedAt.className = "todoCreatedAt";
+    todoPriority.className = "todoPriority";
+    deleteButton.className = "deleteButton";
+    checkBox.className = "checkBox";
+
+    todoText.innerHTML = TasksOfarr[i][0];
+    todoCreatedAt.innerHTML = TasksOfarr[i][1];
+    todoPriority.innerHTML = TasksOfarr[i][2];
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa fa-trash";
+    deleteIcon.setAttribute("aria-hidden", "true");
+    deleteButton.appendChild(deleteIcon);
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.checked = TasksOfarr[i][3];
+
+    todoContainer.append(todoText, todoCreatedAt, todoPriority, checkBox, deleteButton);
+    selector.appendChild(todoContainer);
+
+    deleteButton.onclick = function () {
+        deletedArr.push(todoContainer);
+        selectorView.removeChild(todoContainer);
+        counterFnc();
+        saveTasks();
+    }
+    checkBox.onclick = function () {
+        debugger;
+        if (checkBox.checked) {
+            selectorPerformed.appendChild(todoContainer);
+        } else {
+            selectorView.appendChild(todoContainer);
+        }
+        saveTasks();
+    }
+    counterFnc();
+}
+
+//האירוע והפעולה להחלפת שם הרשימה
 nameOfList.addEventListener("click", alertOfName);
-
-
 function alertOfName() {
     swal({
         title: "Change title list",
@@ -248,4 +279,29 @@ function alertOfName() {
 
 }
 
-
+//האירוע והפעולה למחיקת על המידע
+clear.addEventListener("click", alertAndDelete);
+function alertAndDelete() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+            deleteEverything();
+        }
+    })
+}
+function deleteEverything() {
+    localStorage.clear();
+    location.reload();
+}
